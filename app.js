0d4ddf698131935aabd3e11c5fe3f048a7128d4c
visionxma <visionxma@gmail.com>
2026-07-17 12:49:52 -0300

    Primeiro commit

--- app.js
+++
@@ -1,8 +0,0 @@
-const topbar = document.getElementById('topbar');
-const name = document.querySelector('.name');
-
-// A barra do topo entra assim que o nome sai da tela.
-new IntersectionObserver(
-  ([entry]) => topbar.classList.toggle('is-visible', !entry.isIntersecting),
-  { rootMargin: '-8px 0px 0px 0px' }
-).observe(name);
